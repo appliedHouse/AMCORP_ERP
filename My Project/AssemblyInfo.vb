3633984cda106bd51f62ce4dd4e87064d77e7e2c
Muhammad Aamir Jahangir <info@jahangir.com>
2026-03-05 13:53:54 +0500

    HPLT - 05-MAR-2026. update

--- My Project/AssemblyInfo.vb
+++ My Project/AssemblyInfo.vb
@@ -31,6 +31,6 @@ Imports System.Runtime.InteropServices
 ' You can specify all the values or you can default the Build and Revision Numbers 
 ' by using the '*' as shown below:
 
-<Assembly: AssemblyVersion("2026.03.05.01")>
-<Assembly: AssemblyFileVersion("2026.03.05.01")>
+<Assembly: AssemblyVersion("2026.03.05.02")>
+<Assembly: AssemblyFileVersion("2026.03.05.02")>
 <Assembly: NeutralResourcesLanguage("en-US")>
